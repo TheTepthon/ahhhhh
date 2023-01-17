ed1e48c64c1d87e1305432096c288dcc422d7c39
Source Tepthon <anes12salih@gmail.com>
2023-01-18 00:12:50 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -250,7 +250,7 @@ async def _(event):
                         channel=ch, username=username))
                     await event.client.send_message(event.chat_id, f'''
 - Done ↣ (@{username})
-- By ↣ @iziii - @zbaak !
+- By ↣ @S8Y8S - @N1111V !
 - Hunting History ↣ {Hussenk}
 - Hunting Hour ↣ {Levii}
     ''')
@@ -310,7 +310,7 @@ async def _(event):
                             channel=ch, username=username))
                         await event.client.send_message(event.chat_id, f'''
 - Done ↣ (@{username})
-- By ↣ @iziii - @zbaak !
+- By ↣ @S8Y8S - @N1111V !
 - Hunting History ↣ {Hussenk}
 - Hunting Hour ↣ {Levii}
     ''')
@@ -344,7 +344,7 @@ async def _(event):
                     channel=ch, username=username))
                 await event.client.send_message(event.chat_id, f'''
 - Done ↣ (@{username})
-- By ↣ @iziii - @zbaak !
+- By ↣ @S8Y8S - @N1111V !
 - Hunting History ↣ {Hussenk}
 - Hunting Hour ↣ {Levii}
     ''')
